ef8633d929e8817c57c758c00823a7cb62ddf05c
jangsen <dh7895210@naver.com>
2025-02-19 17:17:44 +0900

    Add database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
+import { DatabaseModule } from './database/database.module';
+import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+
+        RABBITMQ_URL: Joi.string().required(),
+      }),
+    }),
+    OrderModule,
+    DatabaseModule,
+    RabbitmqModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
