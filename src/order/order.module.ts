4af80cbf3892069a1f9938aa6c67458588f79945
jangsen <dh7895210@naver.com>
2025-02-19 18:58:56 +0900

    Add rabbitmq

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -3,9 +3,10 @@ import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
+import { RabbitmqModule } from '../rabbitmq/rabbitmq.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])],
+  imports: [TypeOrmModule.forFeature([Order]), RabbitmqModule],
   controllers: [OrderController],
   providers: [OrderService],
 })
